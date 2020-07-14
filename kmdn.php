f5879a5796deca0ace002c833c744bec90685ff5
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2020-07-14 11:23:22 +0800

    Update kmdn.php
    $p->getAuthor();
    $p->getContent();
    $p->getNextUrl();

--- kmdn.php
+++ kmdn.php
@@ -1,13 +1,13 @@
 <?php
 require_once('simple_html_dom.php');
 $url = 'https://www.kmdn.gov.tw/1117/1271/1275/321006/';
-//$html= file_get_html($url);
-//$find1=$html->find('[class=k_con1]',0);
-//echo $find1;
+
 $p = new Post($url);
-$p->getHeading();
-$p->getDate();
-$p->getAuthor();
+//$p->getHeading();
+//$p->getDate();
+//$p->getAuthor();
+//$p->getContent();
+$p->getNextUrl();
 
 class Post
 {
@@ -28,21 +28,38 @@ class Post
         $this->find1 = $html->find('[class=k_con1]', 0);
     }
 
-    public function getHeading(){
-        $h1=$this->find1->find('h1',0);
+    public function getHeading()
+    {
+        $h1 = $this->find1->find('h1', 0);
         echo strip_tags($h1);
     }
 
-    public function getDate(){
-        $date=$this->find1->find('[class=col-xs-12 date]',0);
+    public function getDate()
+    {
+        $date = $this->find1->find('[class=col-xs-12 date]', 0);
         echo trim(strip_tags($date));
     }
 
-    public function getAuthor(){
-        $tmp=$this->find1->find('[class=col-md-10 word]',0);
-        $tmp=trim(strip_tags($tmp));
-        preg_match('/作者：(.+?)。.+?點閱率：([0-9]+)/u',$tmp,$matches);
+    public function getAuthor()
+    {
+        $tmp = $this->find1->find('[class=col-md-10 word]', 0);
+        $tmp = trim(strip_tags($tmp));
+        preg_match('/作者：(.+?)。.+?點閱率：([0-9]+)/u', $tmp, $matches);
         echo $matches[1];
         echo $matches[2];
+    }
+
+    public function getContent()
+    {
+        $content = $this->find1->find('[class=content]', 0);
+        echo strip_tags($content);
+    }
+
+    public function getNextUrl()
+    {
+        $tmp = $this->find1->find('[class=list_sty]', 1);
+        if (preg_match('/<a href="(.+?)">/', $tmp, $matches)) {
+            echo $matches[1];
+        }
     }
 }
